0af1d37f99b1ddc5fd292e41006ee87d0c6c1bd4
Abdelwahab <mramged936@gmail.com>
2023-06-20 11:54:22 +0300

    Adding Views

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
 
-$config = require "./config.php";
-require "database/Connection.php";
-require "./database/migrations/CreateUsersTable.php";
-require "./database/migrations/CreatePostsTable.php";
-
-$pdo = Connection::make($config['database']);
-CreateUsersTable::usersTable($pdo);
-CreatePostsTable::postsTable($pdo);
+require "./bootstrap.php";
+require "./views/index.view.php";
